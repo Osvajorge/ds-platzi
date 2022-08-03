6a9a9aff413fa88a6b34e13256ba22c9f2fb3f71
Jorge <jorge_osva@hotmail.com>
2022-08-03 14:42:46 -0500

    created 4.py and edited 3.py

--- intemediate-python/3_dictionay_comprenhetions.py
+++ intemediate-python/3_dictionay_comprenhetions.py
@@ -5,7 +5,7 @@ def run():
 
     # mydict = {i : i**2 for i in range (101) if i%3 != 0}
 
-    mydict = {i: round(i**0.5,2) for i in range(11)}
+    mydict = {i: round(i**0.5,2) for i in range(1001)} #dict that contains the first 1000 natural numbers and theirs squares root
     print(mydict)
 if __name__ == '__main__':
     run()
