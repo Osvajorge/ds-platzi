33a401acf5a5d44ccd4016a1cfb921d8635daec8
Jorge <jorge_osva@hotmail.com>
2022-08-03 13:28:26 -0500

    modification of 3.py

--- intemediate-python/3_dictionay_comprenhetions.py
+++ intemediate-python/3_dictionay_comprenhetions.py
@@ -1,8 +1,11 @@
-#create a dict containing the first 100 natural numbers as a keys, and the square of this numbers as a values.
-#this numbers can't be dividet by 3
+#Using dictionary comprehentions 
 def run():
-    mydict = {i : i**2 for i in range (101) if i%3 != 0}
-    print(mydict)
+    #create a dict containing the first 100 natural numbers as a keys, and the square of this numbers as a values.
+    #this numbers can't be divided by 3
 
+    # mydict = {i : i**2 for i in range (101) if i%3 != 0}
+
+    mydict = {i: round(i**0.5,2) for i in range(11)}
+    print(mydict)
 if __name__ == '__main__':
     run()
