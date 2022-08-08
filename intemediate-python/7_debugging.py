a2ea94a83455d374cb1f20ab362d6209bba65687
Jorge <jorge_osva@hotmail.com>
2022-08-08 11:00:38 -0500

    7.py added

---
+++ intemediate-python/7_debugging.py
@@ -0,0 +1,14 @@
+def division(num):
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
+    return divisors
+
+
+
+
+def run ():
+    num = int(input("Enter a number: "))
+    print(division(num))
+    print("The program ended")
+
+if __name__ == '__main__':
+    run()
