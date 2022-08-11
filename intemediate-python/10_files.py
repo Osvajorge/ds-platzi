87c9be80ac2a9d11c05100fba5b2feefcf433f0a
Jorge <jorge_osva@hotmail.com>
2022-08-11 12:04:56 -0500

    10.py added and txts for tha file created

--- intemediate-python/10_files.py
+++ intemediate-python/10_files.py
@@ -1,6 +1,21 @@
+def read():
+    numbers = []
+    with open('./files/numbers.txt', 'r', encoding="utf-8") as f:
+        for line in f:  
+            numbers.append(int(line))
+    print(numbers)
+
+def write():
+    names = ['John', 'Bob', 'Alice', 'Mary', 'Rocío']
+    with open('./files/names.txt', 'a', encoding="utf-8") as f:
+        for name in names:
+            f.write(name + '\n') 
+
+
+
 def run ():
-    pass
+    read()
+    write()
 
 if __name__ == '__main__':
     run()
-    
