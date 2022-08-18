581848881af4585ee2d1e95ecf987d58d810dd85
Jorge <jorge_osva@hotmail.com>
2022-08-18 12:45:01 -0500

    mofification to DATA and new code added to .py, some file deleted

--- python_projects/hangman_game/hangman_game.py
+++ python_projects/hangman_game/hangman_game.py
@@ -1,24 +1,71 @@
 import os
 import variables
 import random
+import data
+
+def get_word(language):
+    if language == 'es':
+        get_word.word = data.ES_WORDS[random.randint(0, len(data.ES_WORDS)-1)]
+    if language == 'en':
+        get_word.word = data.EN_WORDS[random.randint(0, len(data.EN_WORDS)-1)]
+    return get_word.word
+
+def game_screen(word):
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\n")
+        print("The word is: ")
+        print(word)
+        print("\n")
+        print("You have {} tries left".format(variables.TRIES))
+        print("\n")
+        print("The letters you have guessed: ")
+        print(variables.GUESSED)
+        print("\n")
+        guess = input("Enter a letter: ")
+        if guess in variables.GUESSED:
+            print("You have already guessed that letter")
+            continue
+        elif guess in word:
+            variables.GUESSED.append(guess)
+            print("\n")
+            print("Correct")
+            print("\n")
+        else:
+            variables.TRIES -= 1
+            print("\n")
+            print("Incorrect")
+            print("\n")
+        if variables.TRIES == 0:
+            print("You lost")
+            break
+        if "*" not in variables.GUESSED:
+            print("You won")
+            break
+        if variables.TRIES > 0:
+            continue
+        else:
+            break
+    
+        
+    
 
 #hangman game
 def hangman_game():
     title()
     os.system ("echo '{}' | lolcat".format(variables.SUBMENU))
-    #open data file
-    with open('./data.py', 'r', encoding="utf-8") as f:
-        dt = f.read() #read file
+    get_word(menu.chosen_l)
+    game_screen(get_word)
     
-    #
-    while True:
-        wordtoguess = random.choice(dt.split('\n')) #random word from data file
-        print(wordtoguess)
 
-        
+    
 
 
 
+
+
+
+                
 def title():
     os.system('cls' if os.name == 'nt' else 'clear')
     os.system ("echo '{}' | lolcat".format(variables.HANGMAN_TITLE)) #print title and pass it trough the terminal using lolcat
@@ -31,14 +78,16 @@ def menu():
         choice = input("Enter your choice: ")
         if choice == "1":
             title()
-            print("1. English")
-            print("2. Spanish")
+            print("1. Spanish")
+            print("2. English")
             os.system ("echo '{}' | lolcat".format("Press any other key to exit"))
             print("\n")
             language = input("Enter the language in which you want to guess: ")
             if language == "1":
+                menu.chosen_l='es'
                 hangman_game()
             elif language == "2":
+                menu.chosen_l='en'
                 hangman_game()
             break   
         elif choice == "2":
--- python_projects/hangman_game/test.py
+++
@@ -1,7 +0,0 @@
-numbers = [1,3,2,6,7,4,8,9,10,3]
-n = [1,2,3,4,5,6,7,8,9,10]
-
-for i in numbers:
-    for j in n:
-        if i == j:
-            print(i, end=' ')
